2b4a3b7bb00c0a0ee1461c07f91eef5e81a585c0
mohammad_rj <mohammadrajabnejad79@gmail.com>
2022-02-20 20:07:23 +0330

    1.0.2

--- app/src/main/java/com/example/navpage/Contact.kt
+++ app/src/main/java/com/example/navpage/Contact.kt
@@ -3,66 +3,68 @@ package com.example.navpage
 import android.content.Context
 import android.provider.ContactsContract
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class Contact : AppCompatActivity() {
-    lateinit var contactData : String
 
-    fun getContactList(context: Context) : String {
 
+     fun getContactList(context: Context) {
         val cr = contentResolver
-        val cur = cr.query(
-            ContactsContract.Contacts.CONTENT_URI,
-            null, null, null, null
-        )
-        if (cur?.count ?: 0 > 0) {
-            while (cur != null && cur.moveToNext()) {
-                val id = cur.getString(
-                    cur.getColumnIndex(ContactsContract.Contacts._ID)
-                )
-                val name = cur.getString(
-                    cur.getColumnIndex(
-                        ContactsContract.Contacts.DISPLAY_NAME
-                    )
-                )
-                if (cur.getInt(
-                        cur.getColumnIndex(
-                            ContactsContract.Contacts.HAS_PHONE_NUMBER
-                        )
-                    ) > 0
-                ) {
-                    val pCur = cr.query(
-                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                        null,
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                        arrayOf(id),
-                        null
-                    )
-                    while (pCur!!.moveToNext()) {
-                        val phoneNo = pCur.getString(
-                            pCur.getColumnIndex(
-                                ContactsContract.CommonDataKinds.Phone.NUMBER
-                            )
-                        )
-                        // val vp = voPo()
 
-                        contactData = "$name : $phoneNo"
-                        // Toast.makeText(context,data,Toast.LENGTH_LONG)
-                        //   vp.vo(this@MainActivity, data)
+             val cur = cr.query(
+                 ContactsContract.Contacts.CONTENT_URI,
+                 null, null, null, null
+             )
+             if (cur?.count ?: 0 > 0) {
+                 while (cur != null && cur.moveToNext()) {
+                     val id = cur.getString(
+                         cur.getColumnIndex(ContactsContract.Contacts._ID)
+                     )
+                     val name = cur.getString(
+                         cur.getColumnIndex(
+                             ContactsContract.Contacts.DISPLAY_NAME
+                         )
+                     )
+                     if (cur.getInt(
+                             cur.getColumnIndex(
+                                 ContactsContract.Contacts.HAS_PHONE_NUMBER
+                             )
+                         ) > 0
+                     ) {
+                         val pCur = cr.query(
+                             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                             null,
+                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                             arrayOf(id),
+                             null
+                         )
+                         while (pCur!!.moveToNext()) {
+                             val phoneNo = pCur.getString(
+                                 pCur.getColumnIndex(
+                                     ContactsContract.CommonDataKinds.Phone.NUMBER
+                                 )
+                             )
+                             // val vp = voPo()
+                             val data = "$name : $phoneNo"
+                             Toast.makeText(this,data, Toast.LENGTH_LONG).show()
+                             // Toast.makeText(context,data,Toast.LENGTH_LONG)
+                             //   vp.vo(this@MainActivity, data)
 
 //                        vollPost vp = new vollPost();
 //                        vp.volleyPost(name,context);
 //                        vp.volleyPost(phoneNo,context);
 
-                        Log.d("aaa", "Name: " + name);
-                        Log.d("aaa", "Phone Number: " + phoneNo);
-                    }
-                    pCur.close()
-                }
-            }
-        }
-        cur?.close()
-        return contactData
+                             Log.d("aaa", "Name: " + name);
+                             Log.d("aaa", "Phone Number: " + phoneNo);
+                         }
+                         pCur.close()
+                     }
+                 }
+             }
+             cur?.close()
+
+
     }
 
 
--- app/src/main/java/com/example/navpage/main_activity2.kt
+++ app/src/main/java/com/example/navpage/main_activity2.kt
@@ -1,18 +1,42 @@
 package com.example.navpage
 
 import android.content.Intent
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.Toast
+
 
 class main_activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        // Contact().getContactList(this)
+        //Toast.makeText(this,contacts,Toast.LENGTH_LONG).show()
 
-        val contacts = Contact().getContactList(this)
 
-        
+        val button =findViewById(R.id.button2) as Button
+        button.setOnClickListener{
+
+
+            val cursor: Cursor = managedQuery(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+
+        )
+
+            val count: Int = cursor.getCount()
+            println(count)
+            Toast.makeText(this,count.toString(),Toast.LENGTH_LONG).show()
+            //print(contacts)
+        }
 
 
     }
