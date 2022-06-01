b4073670fd62696ab804e02fa086ea79a84b7b2f
mohammad_rj <mohammadrajabnejad79@gmail.com>
2022-06-01 21:26:36 +0430

    1.2.4 // ui edit 3

--- app/src/main/java/com/example/navpage/sing_in.kt
+++ app/src/main/java/com/example/navpage/sing_in.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.EmailAuthProvider
@@ -41,13 +42,13 @@ class sing_in : AppCompatActivity() {
 
 
 
-        email = findViewById(R.id.editTextTextEmailAddress3) as EditText
-        password = findViewById(R.id.editTextTextPassword3) as EditText
+
         loginButton = findViewById(R.id.button_login) as Button
         //registerButtton = findViewById(R.id.button_login) as Button
 
         loginButton.setOnClickListener {
 /*
+
             auth = FirebaseAuth.getInstance()
             auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
                 .addOnCompleteListener { task: Task<AuthResult> ->
@@ -59,19 +60,34 @@ class sing_in : AppCompatActivity() {
 // Get auth credentials from the user for re-authentication. The example below shows
 // email and password credentials but there are multiple possible providers,
 // such as GoogleAuthProvider or FacebookAuthProvider.
-            val user = Firebase.auth.currentUser!!
+            email = findViewById(R.id.editTextTextEmailAddress3) as EditText
+            password = findViewById(R.id.editTextTextPassword3) as EditText
+            auth.signInWithEmailAndPassword(email.toString(),
+                password.toString()
+            ).addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    val intent= Intent(this,main_activity2::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(applicationContext,exception.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+
+    /*        val user = Firebase.auth.currentUser!!
             val credential = EmailAuthProvider
                 .getCredential("user@example.com", "password1234")
                    user.reauthenticate(credential)
                 .addOnCompleteListener { Log.d(TAG, "User re-authenticated.") }
 
+*/
 
 
-           val name = intent.getStringExtra("userId")
-            val intent = Intent(this,main_activity2::class.java)
-            intent.putExtra("userId",name)
-            startActivity(intent)
         }
+        val name = intent.getStringExtra("userId")
+        val intent = Intent(this,main_activity2::class.java)
+        intent.putExtra("userId",name)
+        startActivity(intent)
     }
 }
 
